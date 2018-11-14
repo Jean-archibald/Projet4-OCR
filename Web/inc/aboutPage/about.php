6999d4343f2c11626399cf72d107bf729a3dc8e3
Jean-Archibald <jvjlondon@outlook.com>
2018-11-14 16:54:29 +0100

    font size change

--- Web/inc/aboutPage/about.php
+++ Web/inc/aboutPage/about.php
@@ -1,7 +1,7 @@
 <section id="about" class="about-section text-center">
       <div class="container">
         <div class="row">
-          <div class="col-lg-8 mx-auto">
+          <div class="col-lg-12 mx-auto">
             <h2 class="text-white mb-4">Jean ForteRoche</h2>
             <p class="text-white-50">À l'instar de ses amis de la « bande du Conservatoire » comme Jean-Pierre Marielle et Philippe Noiret,
             Jean ForteRoche s'est peu à peu imposé comme une figure du cinéma français dès les années 1960.
